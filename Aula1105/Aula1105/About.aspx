0c6536fc41bb67592f664d57cf1909b3360be907
Aluno <Aluno@045LAB04017>
2017-05-11 22:21:33 -0300

    Versao final da aula 1105

--- Aula1105/Aula1105/About.aspx
+++ Aula1105/Aula1105/About.aspx
@@ -1,7 +1,12 @@
 ﻿<%@ Page Title="About" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="About.aspx.cs" Inherits="Aula1105.About" %>
 
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
-    <h2><%: Title %>.</h2>
-    <h3>Your application description page.</h3>
+    <h2>Sobre</h2>
+    <h3>Descrição da aplicação</h3>
     <p>Use this area to provide additional information.</p>
+
+    <asp:Calendar ID="calDAtaInicio" runat="server"></asp:Calendar>
+    <br />
+    <asp:TextBox ID="txtData" runat="server"></asp:TextBox>
+    <asp:Button ID="bttSalvar" Cssclass="btn btn-primary" runat="server" Text="Salvar" OnClick="bttSalvar_Click" />
 </asp:Content>
--- Aula1105/Aula1105/About.aspx.cs
+++ Aula1105/Aula1105/About.aspx.cs
@@ -9,9 +9,20 @@ namespace Aula1105
 {
     public partial class About : Page
     {
-        protected void Page_Load(object sender, EventArgs e)
+        protected void Page_Load(object sender, EventArgs e) // evento chamado quando a página é carregada.
         {
+            if (!IsPostBack) // se a página NÃO for de um novo carregamento
 
+                txtData.Text = DateTime.Now.ToString();
+
+
+
+        }
+
+        protected void bttSalvar_Click(object sender, EventArgs e) // evento do botão
+        {
+            DateTime data = calDAtaInicio.SelectedDate;      //variavel data recebe a data selecionada no calendario
+            txtData.Text = data.ToString(); // atribui ao txtbox a data.
         }
     }
 }
